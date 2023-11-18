556b7d70047425cf89a919cc0fcde097a1eaf19c
Ray Huang <ray880301@gmail.com>
2023-11-18 10:37:24 +0300

    first commit

--- src/permissionless-intent-based-relayer.ts
+++ src/permissionless-intent-based-relayer.ts
@@ -1,33 +1,32 @@
 import {
   NewRelayer as NewRelayerEvent,
-  RelayerUpdated as RelayerUpdatedEvent
-} from "../generated/PermissionlessIntentBasedRelayer/PermissionlessIntentBasedRelayer"
-import { NewRelayer, RelayerUpdated } from "../generated/schema"
+  RelayerUpdated as RelayerUpdatedEvent,
+} from "../generated/PermissionlessIntentBasedRelayer/PermissionlessIntentBasedRelayer";
+import { NewRelayer } from "../generated/schema";
 
 export function handleNewRelayer(event: NewRelayerEvent): void {
-  let entity = new NewRelayer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.relayer = event.params.relayer
-  entity.relayerMetadataUri = event.params.relayerMetadataUri
+  const relayer = event.params.relayer;
+  let entity = new NewRelayer(relayer);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.relayer = event.params.relayer;
+  entity.relayerMetadataUri = event.params.relayerMetadataUri;
 
-  entity.save()
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleRelayerUpdated(event: RelayerUpdatedEvent): void {
-  let entity = new RelayerUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.relayer = event.params.relayer
-  entity.newRelayerMetadataUri = event.params.newRelayerMetadataUri
+  const relayer = event.params.relayer;
+  let entity = new NewRelayer(relayer);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.relayerMetadataUri = event.params.newRelayerMetadataUri;
 
-  entity.save()
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
